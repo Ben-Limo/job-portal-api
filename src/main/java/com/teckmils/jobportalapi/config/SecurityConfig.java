55365b1f8b07013d47bf80f757b6d291673cdef4
Ben Limo <bensokiplimo@gmail.com>
2023-01-26 16:02:27 +0300

    create multiple in memory users

--- src/main/java/com/teckmils/jobportalapi/config/SecurityConfig.java
+++ src/main/java/com/teckmils/jobportalapi/config/SecurityConfig.java
@@ -5,6 +5,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -22,5 +25,22 @@ public class SecurityConfig {
                 .httpBasic();
 
         return http.build();
+    }
+    @Bean
+    public InMemoryUserDetailsManager userDetailsManager() {
+        UserDetails admin = User.withDefaultPasswordEncoder()
+                .username("admin")
+                .password("12345")
+                .authorities("admin")
+                .build();
+
+        UserDetails user = User.withDefaultPasswordEncoder()
+                .username("user")
+                .password("12345")
+                .authorities("read")
+                .build();
+
+        return new InMemoryUserDetailsManager(admin, user);
+
     }
 }
