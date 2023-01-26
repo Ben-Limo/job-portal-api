3accb01863c11194cde2ed740843e488e3677450
Ben Limo <bensokiplimo@gmail.com>
2023-01-26 16:10:40 +0300

    use NoOpPasswordEncoder when creating in memory user

--- src/main/java/com/teckmils/jobportalapi/config/SecurityConfig.java
+++ src/main/java/com/teckmils/jobportalapi/config/SecurityConfig.java
@@ -7,8 +7,12 @@ import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.naming.NoPermissionException;
 
 @Configuration
 public class SecurityConfig {
@@ -28,19 +32,22 @@ public class SecurityConfig {
     }
     @Bean
     public InMemoryUserDetailsManager userDetailsManager() {
-        UserDetails admin = User.withDefaultPasswordEncoder()
-                .username("admin")
+        UserDetails admin = User.withUsername("admin")
                 .password("12345")
                 .authorities("admin")
                 .build();
 
-        UserDetails user = User.withDefaultPasswordEncoder()
-                .username("user")
+        UserDetails user = User.withUsername("user")
                 .password("12345")
                 .authorities("read")
                 .build();
 
         return new InMemoryUserDetailsManager(admin, user);
 
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
     }
 }
